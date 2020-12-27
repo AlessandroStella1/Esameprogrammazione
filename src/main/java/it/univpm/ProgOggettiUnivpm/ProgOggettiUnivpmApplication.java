a37e61670b22cf12118c20695162cdef4491e8a6
matteo albanesi <matty.alba@libero.it>
2020-12-27 18:48:15 +0100

    commit iniziale

---
+++ src/main/java/it/univpm/ProgOggettiUnivpm/ProgOggettiUnivpmApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.ProgOggettiUnivpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgOggettiUnivpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgOggettiUnivpmApplication.class, args);
+	}
+
+}
